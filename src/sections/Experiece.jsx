2fa44ddb62d94dcf9b74566cd516f15173bd7297
Ein-shen <nashgregory9@gmail.com>
2026-07-20 21:54:06 +0800

    added Experiences allow to know how i got my experiences and stuff hehe

--- src/sections/Experiece.jsx
+++ src/sections/Experiece.jsx
@@ -1,3 +1,97 @@
+
+
+const experiences = [ 
+
+    {
+        title: "On-the-Job Training (OJT)",
+        period: "December 2025 - February 2026",
+        role: "Software Developer",
+        company: "Jairosoft Inc.",
+        description: "Maintained and enhanced a Learning Management System (LMS) — built new features, fixed bugs, and collaborated in Agile/Scrum sprints.",
+        technology: ["Bubble.io", "Microsoft Teams", "DevOps", "Bubble Version Control"],
+        current: false,
+    },
+]
+
+
+
+
+
+
 export const Experience = () => {
-    return <section></section>
+    return (
+    
+    
+    <section 
+        id="experience"
+        className="py-2 relative overflow-hidden">
+            <div
+                className="absolute top-1/2 left-1/4 w-96 h-96 bg-primary/5 rounded-full
+                blur-3xl -translate-y-1/2"
+            />
+
+            <div className="container mz-auto px-6 relative z-10">
+
+
+                {/* section header */}
+                <div className="max-w-3xl mb-16"> 
+                    <span className="text-4xl md:text-5xl font-bold leading-tight animate-fade-in animation-delay-100 text-secondary-foreground">
+                         Career journey
+                         </span>
+                    <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground"
+                        >
+                        Experience that <span className="font-serif italic font-normal text-white">
+                            {" "}
+                            speaks volume</span>
+                    </h2>
+
+                    <p 
+                        className="text-muted-foreground animate-fade-in animation-delay-200"
+                    >
+                        Hi, I'm an aspiring Software Engineer with a passion for building clean, functional, and user-friendly digital experiences. I enjoy turning ideas into real products, whether that means designing an interface, writing backend logic, or optimizing how data flows through an application.
+                    </p>
+                </div>
+
+                {/* Timeline */}
+
+                <div className="relative">
+                    <div className="timeline-glow absolute left-0 md:left-1/2 top-0 botttom-0 w-[2px] bg-gradient-to-b from-primary/70 via-primary/30 to-transparent md:-translate-x-1/2 shadow-(0_0_25px_rgba(32,178,166,0.8)"/>
+                
+                {/* experience items */}
+                <div className="space-y-12">
+                    {experiences.map((exp, idx) => (
+                        <div key={idx} className=" relative grid md:grid-cols-2 gap-8 animate-fade-in"
+                            style={{animationDelay: `${(idx + 1) * 100}ms `}}
+                            >
+                            {/* This is the timeline Dot */}
+                            <div className="absolute left-0 md:left-1/2 top-0 w-3 h-3 bg-primary rounded-full -translate-x-1/2 ring-4 ring-background z-10"> 
+                            {exp.current && <span  className="absolute inset-0 rounded-full bg-primary animate-ping opacity-75"/>}
+                            </div>
+
+                            {/* content */}
+                            <div className={`pl-8 md:pl-0 ${idx % 2 === 0 ? "md:pr-16 md:text-right" : "md:col-start-2 md:pl-16"}`}>
+                                <div className={`glass p-6 rounded-2xl border border-primary/30 hover:border-primary/50 trasition-all duration-500`}>
+                                    <span className="text-sm text-primary font-medium">{exp.period}</span>
+                                    <h3 className="text-xl font-semibold mt-2">{exp.role}</h3>
+                                    <p className="text-muted-foreground">{exp.company}</p>
+                                    <p className="text-sm text-muted-foreground mt-4">{exp.description}</p>
+                                    <div className={`flex flex-wrap gap-2 mt-4 ${idx % 2 === 0 ? "md:justify-end" : ""}`}>
+                                        {exp.technology.map((tech, indxtech) => (
+                                            <span key={indxtech} className="px-3 py-1 bg-surface text-xs rounded-full text-muted-foreground"
+                                            >
+                                            {tech}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                </div>
+            </div>
+        </section>
+
+
+    )
 }
