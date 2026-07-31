f294125f1184f543922765848399ee1853b60ee0
Ein-shen <nashgregory9@gmail.com>
2026-07-31 22:06:11 +0800

    Change the file Outide to Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { Certificates } from "./sections/Certificates"
 import { Cv } from "./away/Cv"
 import { View_projects } from "./away/View_projects"
 import { Toplok } from "./away/Toplok"
-import { Outside } from "./sections/Outside"
+import { Gallery } from "./sections/Gallery"
 import { ThemeProvider } from "./sections/ThemeContext"
 
 function App() {
@@ -32,7 +32,7 @@ function App() {
                 <Experience />
                 <Education />
                 <Certificates />
-                <Outside />
+                <Gallery />
               </>
             } />
             
--- src/sections/Gallery.jsx
+++ src/sections/Gallery.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const Outside = () => {
+export const Gallery = () => {
   return (
      <section className="py-2 relative overflow-hidden scroll-mt-24">
 
