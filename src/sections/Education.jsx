ab2092bf5a4fbe4158761647c3ba91e2005e7d6a
Ein-shen <nashgregory9@gmail.com>
2026-09-21 18:58:09 +0800

    added the same style infrom experience to eduaction mobile

--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -21,18 +21,18 @@ export const Education = () => {
                         </h1>
                     </div>
 
-                    <div className="relative">
+                    <div className="relative  ">
                         {Educ.map((edu, idx) => (
-                            <div key={idx} className="pb-10">
-                                <div className="flex flex-col items-center  rounded-2xl p-6 transition-all duration-500 sm:flex-row sm:items-start ">
+                            <div key={idx} className="pb-10 ">
+                                <div className="flex flex-col items-center space-y-4    rounded-2xl p-6 transition-all duration-500 sm:flex-row sm:items-start ">
                                     <span className="theme-muted shrink-0 text-muted-foreground font-mono text-xs font-medium sm:w-24 sm:text-sm ">
                                         {edu.period}
                                     </span>
 
                                     <div className="flex flex-col items-center w-full">
                                         <h3 className="text-xl font-semibold text-center">{edu.course}</h3>
-                                        <p className="text-xs text-muted-foreground pt-3 text-center">{edu.school}</p>
-                                        <p className="text-md text-muted-foreground pt-3 text-center">{edu.award}</p>
+                                        <p className="text-xs text-muted-foreground pt-4 text-center">{edu.school}</p>
+                                        <p className="text-md text-muted-foreground pt-4 text-center">{edu.award}</p>
                                     </div>
                                 </div>
                             </div>
