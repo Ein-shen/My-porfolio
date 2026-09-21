b0c56c7048979a4d6fc77c3039d43558c6096556
Ein-shen <nashgregory9@gmail.com>
2026-09-21 18:17:10 +0800

    adjust the color grade

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -55,7 +55,7 @@ const projects = [
   {
     title: "Stock-Trading",
     description:
-      "A web app that lets users manage a virtual stock portfolio and look up real-time stock prices.",
+      "A full-stack web app that lets users manage a virtual stock portfolio and look up real-time stock prices.",
     image: "/fi.png",
     tags: ["Python", "Flask", "SQLite", "Jinja", "HTML", "CSS", "Git",],
     github: "https://github.com/Ein-shen/Stock-Trading",
